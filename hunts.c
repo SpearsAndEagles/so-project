f70257f3945df7a67cf325ae3320922d54f03dbd
Dragan Iulian <iulianalexandrudragan@gmail.com>
2025-04-04 17:00:15 +0300

    feat: implement minimal create_hunt() implementation

---
+++ hunts.c
@@ -0,0 +1,33 @@
+#include "hunts.h"
+#include "helpers.h"
+#include <fcntl.h>
+#include <errno.h>
+
+int create_hunt(const char *hunt_id)
+{
+    if (mkdir(hunt_id, 0755) < 0)
+    {
+        if (errno)
+        {
+            perror("Cannot create directory");
+            return -1;
+        }
+    }
+    char treasure_path[256];
+    snprintf(treasure_path, sizeof(treasure_path), "./%s/%s", hunt_id, TREASURE_FILE);
+    int fd = open(treasure_path, O_RDWR | O_CREAT, 0644);
+    if (fd < 0)
+    {
+        perror("Cannot create treasure file");
+        return -1;
+    }
+    close(fd);
+}
+int remove_hunt(const char *hunt_id)
+{
+    return 0;
+}
+int list_treasures(const char *hunt_id)
+{
+    return 0;
+}
--- hunts.h
+++ hunts.h
@@ -1,6 +1,8 @@
 #ifndef HUNTS_H
 #define HUNTS_H
 
+#define TREASURE_FILE "treasures.dat"
+
 int create_hunt(const char *hunt_id);
 int remove_hunt(const char *hunt_id);
 int list_treasures(const char *hunt_id);
