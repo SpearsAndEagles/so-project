283ff10316e32f243f43c3207b81880022969dc2
Dragan Iulian <iulianalexandrudragan@gmail.com>
2025-04-10 12:45:07 +0300

    fix: add empty function implementations for treasures.h

---
+++ treasures.c
@@ -0,0 +1,14 @@
+#include "treasures.h"
+
+int add_treasure(const char *hunt_id, Treasure *treasure)
+{
+    return 0;
+}
+int view_treasure(const char *hunt_id, int treasure_id)
+{
+    return 0;
+}
+int remove_treasure(const char *hunt_id, int treasure_id)
+{
+    return 0;
+}
