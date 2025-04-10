d709d8bdd6f50dace250aedcfce9e16adc4a85ac
Dragan Iulian <iulianalexandrudragan@gmail.com>
2025-04-10 12:58:26 +0300

    feat: implement hunt removal

--- hunts.c
+++ hunts.c
@@ -1,5 +1,6 @@
 #include "hunts.h"
 #include "helpers.h"
+#include <ftw.h>
 
 int create_hunt(const char *hunt_id)
 {
@@ -20,11 +21,35 @@ int create_hunt(const char *hunt_id)
         return -1;
     }
     close(fd);
-}
-int remove_hunt(const char *hunt_id)
-{
     return 0;
 }
+
+static int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    int ret = remove(fpath);
+    if (ret)
+        perror(fpath);
+    return ret;
+}
+
+static int remove_directory_recursive(const char *path)
+{
+    return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+}
+
+int remove_hunt(const char *hunt_id)
+{
+    if (remove_directory_recursive(hunt_id) < 0)
+    {
+        fprintf(stderr, "Failed to remove hunt %s\n", hunt_id);
+        return -1;
+    }
+    char link_name[256];
+    snprintf(link_name, sizeof(link_name), "logged_hunt-%s", hunt_id);
+    unlink(link_name);
+    return 0;
+}
+
 int list_treasures(const char *hunt_id)
 {
     return 0;
