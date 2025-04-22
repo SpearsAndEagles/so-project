7a1f443a2522277df6897977f2e36436bdb5bcfc
Dragan Iulian <iulianalexandrudragan@gmail.com>
2025-04-22 14:45:48 +0300

    feat: implement remove_treasure()

--- treasures.c
+++ treasures.c
@@ -9,8 +9,8 @@ short ask_and_create_treasure(Treasure *treasure)
     printf("Enter latitude: ");
     while (scanf("%f", &treasure->latitude) != 1)
     {
-        printf(""
-               "Invalid input. Please enter a valid latitude: \n");
+        printf(
+            "Invalid input. Please enter a valid latitude: \n");
     }
     printf("Enter longitude: ");
     while (scanf("%f", &treasure->longitude) != 1)
@@ -79,7 +79,7 @@ void print_treasure(Treasure *treasure)
 
 int view_treasure(const char *hunt_id, int treasure_id)
 {
-char treasure_path[256];
+    char treasure_path[256];
     snprintf(treasure_path, sizeof(treasure_path), "./%s/%s", hunt_id, TREASURE_FILE);
     int fd = open(treasure_path, O_RDONLY);
     if (fd < 0)
@@ -108,5 +108,73 @@ char treasure_path[256];
 }
 int remove_treasure(const char *hunt_id, int treasure_id)
 {
-    return 0;
-}
+    char treasure_path[256], temp_path[256];
+    snprintf(treasure_path, sizeof(treasure_path), "./%s/%s", hunt_id, TREASURE_FILE);
+    snprintf(temp_path, sizeof(temp_path), "./%s/temp.dat", hunt_id);
+    int fd = open(treasure_path, O_RDONLY);
+    if (fd < 0)
+    {
+        perror("open treasure file");
+        return -1;
+    }
+    int temp_fd = open(temp_path, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (temp_fd < 0)
+    {
+        perror("open temp file");
+        close(fd);
+        return -1;
+    }
+    Treasure treasure;
+    int removed = 0;
+    while (read(fd, &treasure, sizeof(Treasure)) == sizeof(Treasure))
+    {
+        if (treasure.id == treasure_id)
+        {
+            removed = 1;
+            continue;
+        }
+        if (write(temp_fd, &treasure, sizeof(Treasure)) != sizeof(Treasure))
+        {
+            perror("write to temp file");
+            close(fd);
+            close(temp_fd);
+            return -1;
+        }
+    }
+    close(fd);
+    close(temp_fd);
+    if (!removed)
+    {
+        printf("Treasure with ID %d not found\n", treasure_id);
+        remove(temp_path);
+        return -1;
+    }
+    if (rename(temp_path, treasure_path) < 0)
+    {
+        perror("rename temp file");
+        return -1;
+    }
+    char details[256];
+    snprintf(details, sizeof(details), "Removed treasure with ID %d", treasure_id);
+    log_operation(hunt_id, "REMOVE_TREASURE", details);
+
+    int list_treasures(const char *hunt_id)
+    {
+        char treasure_path[256];
+        snprintf(treasure_path, sizeof(treasure_path), "./%s/%s", hunt_id, TREASURE_FILE);
+        int fd = open(treasure_path, O_RDONLY);
+        if (fd < 0)
+        {
+            perror("open treasure file");
+            return -1;
+        }
+        Treasure treasure;
+        printf("Treasures in hunt %s:\n", hunt_id);
+        while (read(fd, &treasure, sizeof(Treasure)) == sizeof(Treasure))
+        {
+            print_treasure(&treasure);
+            printf("\n");
+        }
+        close(fd);
+        return 0;
+    }
