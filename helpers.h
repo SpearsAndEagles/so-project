9ad34f9f186854b2b928b0025f9a59e3e385f049
Dragan Iulian <iulianalexandrudragan@gmail.com>
2025-05-05 12:58:37 +0300

    chore: add header guard to helpers.h

--- helpers.h
+++ helpers.h
@@ -1,3 +1,6 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -6,9 +9,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 #define COMMAND(a) strcmp(command, a) == 0
 
 #define TREASURE_FILE "treasures.dat"
 
 void exit_with_error(const char *message, char show_usage);
+
+#endif
